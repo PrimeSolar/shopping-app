710f07c47f81e4049407664903ddab7678c57abd
I am <88476416+PrimeSolar@users.noreply.github.com>
2026-05-21 02:07:15 +0300

    Improve the functional logic; use "const"; rename class and variable
    * Improved the "Scroll to top" button component's functional logic.
    * Used "const" instead of implicit global variable declaration to prevent scope pollution, enforce immutability, and improve code clarity and maintainability.
    * Renamed the class and the variable using pascal case.
    * Created a commentary for improved documentation.

--- scripts/components.js
+++ scripts/components.js
@@ -17,19 +17,28 @@
  */
 
 /** The scroll to top button. */
-class scrollToTop extends HTMLElement {
+class ScrollToTop extends HTMLElement {
+  /**
+   * The script includes detailed comments
+   * to support stakeholders with varying JS knowledge.
+   */
   connectedCallback() {
-    this.innerHTML += `
-    <a href="#" class="to-top" aria-label="Scroll to top" title="Scroll to top">
+    this.innerHTML = `
+    <a class="to-top" aria-label="Scroll to top" title="Scroll to top">
       <svg width="50px" height="50px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="12" cy="12" r="11.5" fill="none" stroke="#fff" stroke-width="1"/>
-        <path d="M6 15L12 9L18 15" stroke="#fff" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"/>
+        <circle cx="12" cy="12" r="11.5" fill="none" stroke="#ffffff" stroke-width="1"/>
+        <path d="M6 15L12 9L18 15" stroke="#ffffff" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"/>
       </svg>
     </a>
     `;
+
+    this.querySelector(".to-top").addEventListener("click", (event) => {
+      event.preventDefault();
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    });
   }
 }
-customElements.define("to-top", scrollToTop);
+customElements.define("to-top", ScrollToTop);
 
 document.querySelector("body").appendChild(document.createElement("to-top"));
 
@@ -43,7 +52,7 @@ window.addEventListener("scroll", () => {
 });
 
 /** The footer. */
-footerContainer = document.querySelector("footer");
+const FooterContainer = document.querySelector("footer");
 const year = new Date().getFullYear();
 function footerContainerF(e) {
   e &&
@@ -52,6 +61,6 @@ function footerContainerF(e) {
       <p>Copyright © <a href="https://primesolar.github.io/web-developer/" rel="noopener noreferrer">Vladislav Kazantsev</a> ${year}</p>
       <a type="button" class="bmc-button" href="https://www.buymeacoffee.com/CocaCola" target="_blank" rel="noopener noreferrer" aria-label="Buy me a coffee" role="button">☕ Buy me a coffee</a>`);
 }
-footerContainerF(footerContainer);
+footerContainerF(FooterContainer);
 
 console.log("components.js is completed");
